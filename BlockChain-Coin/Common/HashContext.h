c0dcf15c5802b42f77cd5f21de4e9456a304a135
Maxime Bornemann <contact@maximebornemann.com>
2018-02-08 15:38:02 +0100

    🚧 Add Cryptonight related libs

---
+++ BlockChain-Coin/Common/HashContext.h
@@ -0,0 +1,15 @@
+//
+//  HashContext.h
+//  BlockChain-Coin
+//
+//  Created by Maxime Bornemann on 08/02/2018.
+//  Copyright © 2018 BlockChain-Coin.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HashContext : NSObject
+
+- (NSData * _Nonnull)hashData:(NSData * _Nonnull)data;
+
+@end
---
+++ BlockChain-Coin/Common/HashContext.mm
@@ -0,0 +1,40 @@
+//
+//  HashContext.mm
+//  BlockChain-Coin
+//
+//  Created by Maxime Bornemann on 08/02/2018.
+//  Copyright © 2018 BlockChain-Coin.net. All rights reserved.
+
+#import "HashContext.h"
+#import "Cryptonight/cryptonight.h"
+
+@interface HashContext()
+
+@property (nonatomic, readonly) struct cryptonight_ctx *ctx;
+
+@end
+
+@implementation HashContext
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _ctx = (struct cryptonight_ctx*)malloc(sizeof(struct cryptonight_ctx));
+    }
+    
+    return self;
+}
+    
+- (void)dealloc {
+    if (_ctx) {
+        free(_ctx);
+        _ctx = NULL;
+    }
+}
+
+- (NSData * _Nonnull)hashData:(NSData * _Nonnull)data {
+    NSMutableData *output = [NSMutableData dataWithLength:32];
+    cryptonight_hash_ctx(output.mutableBytes, data.bytes, self.ctx);
+    return output;
+}
+
+@end
